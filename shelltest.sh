4845e68ec929e7b83f04f6ffc6d871edacc409c5
Qqkyu <kulmisiu007@gmail.com>
2022-02-25 23:08:33 +0100

    Add placing shelltests in directories support

--- shelltest.sh
+++ shelltest.sh
@@ -7,10 +7,26 @@ tests_path="$shelltests_path/tests/"
 for src_path in $src_paths
 do
   [ -e "$src_path" ] || continue
-  src_path_without_extension=${src_path%%.*}
-  src_name=${src_path_without_extension##*/}
-  compile_program="stack run necklace-exe ${src_path}"
-  eval "$compile_program"
-  run_test="shelltest ${tests_path}${src_name}.test"
-  eval "$run_test"
+  if [[ -d $src_path ]]; then
+    nested_shelltest="${src_path}/*"
+    for nested_src_path in $nested_shelltest
+    do
+      nested_src_path_without_extension=${nested_src_path%%.*}
+      test_path=${nested_src_path_without_extension##*src/}
+      compile_program="stack run necklace-exe ${nested_src_path}"
+      eval "$compile_program"
+      run_test="shelltest ${tests_path}${test_path}.test"
+      eval "$run_test"
+    done
+  elif [[ -f $src_path ]]; then
+    src_path_without_extension=${src_path%%.*}
+    src_name=${src_path_without_extension##*/}
+    compile_program="stack run necklace-exe ${src_path}"
+    eval "$compile_program"
+    run_test="shelltest ${tests_path}${src_name}.test"
+    eval "$run_test"
+  else
+    echo "Invalid path"
+    exit 1
+  fi
 done
