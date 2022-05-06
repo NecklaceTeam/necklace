8c30a1b7c3407d8c4e78ea6ca811877d60ed6df4
Qqkyu <kulmisiu007@gmail.com>
2022-05-06 21:32:03 +0200

    Improve tests output

--- shelltest.sh
+++ shelltest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 
 shelltests_path="$(dirname $(realpath $0))/shelltests"
 src_paths="$shelltests_path/src/*"
@@ -20,16 +20,18 @@ do
       test_path=${nested_src_path_without_extension##*src/}
       compile_program="stack run necklace-exe ${nested_src_path}"
       eval "$compile_program"
-      run_test="shelltest ${tests_path}${test_path}.test"
+      run_test="shelltest ${tests_path}${test_path}.test -c | head -n -5 | sed --unbuffered -e 's/\(.*OK.*\)/\o033[32m\1\o033[39m/' -e 's/\(.*Failed.*\)/\o033[31m\1\o033[39m/'"
       eval "$run_test"
+      echo " "
     done
   elif [[ -f $src_path ]]; then
     src_path_without_extension=${src_path%%.*}
     src_name=${src_path_without_extension##*/}
     compile_program="stack run necklace-exe ${src_path}"
     eval "$compile_program"
-    run_test="shelltest ${tests_path}${src_name}.test"
+    run_test="shelltest ${tests_path}${src_name}.test -c | head -n -5 | sed --unbuffered -e 's/\(.*OK.*\)/\o033[32m\1\o033[39m/' -e 's/\(.*Failed.*\)/\o033[31m\1\o033[39m/'"
     eval "$run_test"
+    echo " "
   else
     echo "Invalid path"
     exit 1
