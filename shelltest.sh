b877244470d82d768ec05ffdb4d07620e562445d
Qqkyu <kulmisiu007@gmail.com>
2022-05-22 12:57:26 +0200

    Add invalid programs integration tests

--- shelltest.sh
+++ shelltest.sh
@@ -19,20 +19,19 @@ do
     do
       nested_src_path_without_extension=${nested_src_path%%.*}
       test_path=${nested_src_path_without_extension##*src/}
-      compile_program="stack run necklace-exe ${nested_src_path}"
+      compile_program="stack run necklace-exe ${nested_src_path} scriptname >/dev/null 2>/dev/null"
       eval "$compile_program"
       run_test="shelltest ${tests_path}${test_path}.test -c | head -n -5 | sed --unbuffered -e 's/\(.*OK.*\)/\o033[32m\1\o033[39m/' -e 's/\(.*Failed.*\)/\o033[31m\1\o033[39m/'"
       eval "$run_test"
-      echo " "
     done
+    echo " "
   elif [[ -f $src_path ]]; then
     src_path_without_extension=${src_path%%.*}
     src_name=${src_path_without_extension##*/}
-    compile_program="stack run necklace-exe ${src_path}"
+    compile_program="stack run necklace-exe ${src_path} scriptname >/dev/null 2>/dev/null"
     eval "$compile_program"
     run_test="shelltest ${tests_path}${src_name}.test -c | head -n -5 | sed --unbuffered -e 's/\(.*OK.*\)/\o033[32m\1\o033[39m/' -e 's/\(.*Failed.*\)/\o033[31m\1\o033[39m/'"
     eval "$run_test"
-    echo " "
   else
     echo "Invalid path"
     exit 1
